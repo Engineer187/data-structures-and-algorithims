2d0c3b691f1df3e0d9e3f0e3f4c49dc5dd5b21c7
Engineer187 <its.krishmistry@gmail.com>
2026-02-08 18:01:40 +0000

    Add files via upload

---
+++ student class.py
@@ -0,0 +1,22 @@
+class student():
+    def __init__(self):
+        self.name="krish"
+        self.avrg="100"
+        self.year="10"
+    def display(self):
+        print(self.name)
+        print(self.avrg)
+        print(self.year)
+student1=student()
+student1.display()
+class car():
+    def __init__(self):
+        self.brand="bmw"
+        self.zetosix="6"
+        self.year="10"
+    def display(self):
+        print(self.brand)
+        print(self.zetosix)
+        print(self.year)
+car1=car()
+car1.display()
