907dab97aa62aeeb6d9c0f59210ab52188d29c28
Engineer187 <its.krishmistry@gmail.com>
2026-03-15 18:08:47 +0000

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -22,6 +22,39 @@ for i in range(len(l)):
     temp=l[i]
     l[i]=s
     l[index]=temp
-    print(l)
-
-
+print(l)
+#buble sort
+l2=[5,2,7,9,1,6,3,4,10,8]
+for x in range (len(l2)):
+    for y in range (len(l2)-x-1):
+        v1=l2[y]
+        v2=l2[y+1]
+        if v1>v2:
+            temp1=l2[y]
+            l2[y]=l2[y+1]
+            l2[y+1]=temp1
+    print(l2)
+#sincoserion sort
+l3=[5,2,7,9,1,6,3,4,10,8]
+"""
+l4=[1]
+m=l3[0]
+print(m)
+l4[0]=m
+for i in range (len(l3)-1):
+    m=l3[i+1]
+    for x in range(len(l4)):
+        c=l4[x]
+        if c<m:
+            l4[x-1]=m
+print(l4)
+"""
+print("insersion sort")
+for i in range(1,len(l3)):
+    key=l3[i]
+    j=i-1
+    while j>=0 and key<l3[j]:
+        l3[j+1]=l3[j]
+        j=j-1
+    l3[j+1]=key
+    print(l3)
